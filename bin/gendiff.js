8c06ce8c394b6c91830b4ad1dcac7ed8a923fea6
g.babaeva <g.babaeva@fun-box.ru>
2022-01-19 22:25:50 +0700

    setup simple cli commands

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { readFile } from 'fs/promises';
+
+const { version } = JSON.parse(await readFile(new URL('../package.json', import.meta.url)));
+
+const program = new Command();
+
+program
+  .version(version)
+  .description('Compare two configuration files and shows a difference.')
+  .parse(process.argv);
