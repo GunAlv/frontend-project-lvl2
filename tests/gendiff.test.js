f2642172442d89be37a6edcf1da27d5a40fed41e
g.babaeva <g.babaeva@fun-box.ru>
2022-01-30 15:29:01 +0700

    add plain format test

--- tests/gendiff.test.js
+++ tests/gendiff.test.js
@@ -3,6 +3,8 @@ import fs from 'fs';
 
 import gendiff from '../src/gendiff.js';
 
+import { OUTPUT_FORMATS } from '../src/constants.js';
+
 function getFixtureFile(file) {
   return `${__dirname}/../fixtures/${file}`;
 }
@@ -39,5 +41,34 @@ describe('gendiff stylish format output', () => {
     const expected = getExpectedFile('expected-stylish.txt');
 
     expect(gendiff(config1, config2)).toEqual(expected)
+  });
+});
+
+describe('gendiff plain format output', () => {
+  it('json files', () => {
+    const config1 = getFixtureFile('config1.json');
+    const config2 = getFixtureFile('config2.json');
+
+    const expected = getExpectedFile('expected-plain.txt');
+
+    expect(gendiff(config1, config2, OUTPUT_FORMATS.PLAIN)).toEqual(expected)
+  });
+
+  it('yml files', () => {
+    const config1 = getFixtureFile('config1.yml');
+    const config2 = getFixtureFile('config2.yml');
+
+    const expected = getExpectedFile('expected-plain.txt');
+
+    expect(gendiff(config1, config2, OUTPUT_FORMATS.PLAIN)).toEqual(expected)
+  });
+
+  it('yaml files', () => {
+    const config1 = getFixtureFile('config1.yaml');
+    const config2 = getFixtureFile('config2.yaml');
+
+    const expected = getExpectedFile('expected-plain.txt');
+
+    expect(gendiff(config1, config2, OUTPUT_FORMATS.PLAIN)).toEqual(expected)
   });
 });
