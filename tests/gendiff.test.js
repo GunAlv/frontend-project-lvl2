131047a764dbc5c14e1222a04f0d04dcb1dc4f33
g.babaeva <g.babaeva@fun-box.ru>
2022-01-30 15:40:47 +0700

    add output json test

--- tests/gendiff.test.js
+++ tests/gendiff.test.js
@@ -72,3 +72,32 @@ describe('gendiff plain format output', () => {
     expect(gendiff(config1, config2, OUTPUT_FORMATS.PLAIN)).toEqual(expected)
   });
 });
+
+describe('gendiff json format output', () => {
+  it('json files', () => {
+    const config1 = getFixtureFile('config1.json');
+    const config2 = getFixtureFile('config2.json');
+
+    const expected = getExpectedFile('expected-json.txt');
+
+    expect(gendiff(config1, config2, OUTPUT_FORMATS.JSON)).toEqual(expected)
+  });
+
+  it('yml files', () => {
+    const config1 = getFixtureFile('config1.yml');
+    const config2 = getFixtureFile('config2.yml');
+
+    const expected = getExpectedFile('expected-json.txt');
+
+    expect(gendiff(config1, config2, OUTPUT_FORMATS.JSON)).toEqual(expected)
+  });
+
+  it('yaml files', () => {
+    const config1 = getFixtureFile('config1.yaml');
+    const config2 = getFixtureFile('config2.yaml');
+
+    const expected = getExpectedFile('expected-json.txt');
+
+    expect(gendiff(config1, config2, OUTPUT_FORMATS.JSON)).toEqual(expected)
+  });
+});
